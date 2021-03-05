7be267e4223ffc965a6f9e5e03b51a3668a0b092
Martin Balke <Mrtnbalke@gmail.com>
2021-03-05 11:28:13 -0800

    commit before switching over to next

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import request from 'superagent';
-import { Grid } from '@chakra-ui/react'
+import { Grid, CircularProgress, Box, Button } from '@chakra-ui/react'
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 import UserCard from './UserCard'
 
 function Pagination() {
@@ -25,32 +26,55 @@ function Pagination() {
 		}
 		getUserData();
 	}, []);
-
+  if (!userData.length) return (
+		<Box h='100vh' w='100vw' d='flex' justifyContent='center' alignItems='center'>
+			<CircularProgress isIndeterminate color='green.300' />
+		</Box>
+	);
 	return (
-    <Grid className='pagination' templateColumns='repeat(5, 1fr)' gap={6} margin='1rem'>
-			{userData.slice(activePage * 10, activePage * 10 + 10).map((user) => {
-				return (
-          <UserCard picture={user.picture} name={user.name} age={user.age} username={ user.username}/>
-				);
-			})}
-			<div>
-				<button onClick={() => setActivePage(activePage + 1)}>+</button>
+		<Box
+			d='flex'
+			flexDirection='column'
+			className='pagination'
+			margin='4'
+			p='2'
+		>
+			<Grid templateColumns='repeat(5, 1fr)' gap={6} margin='4' p='2'>
+				{userData.slice(activePage * 10, activePage * 10 + 10).map((user) => {
+					return (
+						<UserCard
+							picture={user.picture}
+							name={user.name}
+							age={user.age}
+							username={user.username}
+						/>
+					);
+				})}
+			</Grid>
+			<Box d='flex' position='asbolute' alignSelf='center' bottom='0'>
+				<Button
+					onClick={() => setActivePage(activePage - 1)}
+					m='1'
+					leftIcon={<ArrowLeftIcon />}
+				/>
 				{pageNumbers.map((number) => (
-					<button
+					<Button
+						m='1'
 						key={number}
-						style={
-							activePage === number
-								? { backgroundColor: 'blue', color: 'white' }
-								: { backgroundColor: '' }
-						}
+            bg={ number === activePage ? 'lightskyblue' : '' }
+            color={ number === activePage ? 'white' : '' }
 						onClick={() => setActivePage(number)}
 					>
 						{number}
-					</button>
+					</Button>
 				))}
-				<button onClick={() => setActivePage(activePage - 1)}>-</button>
-			</div>
-		</Grid>
+				<Button
+					m='1'
+					onClick={() => setActivePage(activePage + 1)}
+					rightIcon={<ArrowRightIcon />}
+				/>
+			</Box>
+		</Box>
 	);
 }
 
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {Box, Image, Text } from '@chakra-ui/react'
-
+import { CalendarIcon} from '@chakra-ui/icons'
 const UserCard = ({name, picture, username, age }) => {
   return (
-    <Box key={name} d='flex' flexDirection='column' borderRadius='lg' overflow='hidden'>
-      <Image src={picture} alt='personal photograph' />
-      <Text as='h3' fontSize='lg' fontWeight='bold'>{username}</Text>
-      <Text as='h3' fontSize='md'>{name}</Text>
-      <Text as='p' fontSize='sm' fontWeight='black' alignSelf='flex-end'>{age}</Text>
-    </Box>
-  );
+		<Box
+			key={name}
+			d='flex'
+			flexDirection='column'
+			borderRadius='lg'
+			overflow='hidden'
+		>
+			<Image src={picture} alt='personal photograph' objectFit='cover' />
+			<Text as='h3' fontSize='lg' fontWeight='bold' pl='2'>
+				{username}
+			</Text>
+			<Text as='h3' fontSize='md' pl='2'>
+				{name}
+			</Text>
+			<Box d='flex' alignItems='center' justifyContent='flex-end'>
+				<CalendarIcon color='teal' />
+				<Text as='p' fontSize='sm' fontWeight='black' pl='2' color='green.400'>
+					{age}
+				</Text>
+			</Box>
+		</Box>
+	);
 };
 
 export default UserCard;
